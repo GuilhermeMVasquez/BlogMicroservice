ea6162ed7898d974ec931de37c8fdbc0af99a0dd
GuilhermeMVasquez <guilhermemalgarizi@gmail.com>
2023-07-08 23:07:29 -0300

    post service 3

--- posts/index.js
+++ posts/index.js
@@ -23,7 +23,7 @@ app.post('/posts', async (req, res) => {
         title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://192.168.1.104:4005/events', {
         type: 'PostCreated',
         data: {
             id,
