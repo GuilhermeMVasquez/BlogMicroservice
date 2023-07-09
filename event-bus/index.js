dd09b1b8c6e0a4de93583723323739eeab8d14e7
GuilhermeMVasquez <guilhermemalgarizi@gmail.com>
2023-07-08 23:07:51 -0300

    event bus 2

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,15 +8,19 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {
+    axios.post('http://192.168.1.104:4000/events', event).catch((err) => {
         console.log(err.message);
     });    
 
-    axios.post('http://localhost:4001/events', event).catch((err) => {
+    axios.post('http://192.168.1.104:4001/events', event).catch((err) => {
         console.log(err.message);
     });
 
-    axios.post('http://localhost:4002/events', event).catch((err) => {
+    axios.post('http://192.168.1.104:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
+
+    axios.post('http://192.168.1.104:4003/events', event).catch((err) => {
         console.log(err.message);
     });
 
