d013a47202c8b084da5842a613d56c705fd3aac8
GuilhermeMVasquez <guilhermemalgarizi@gmail.com>
2023-07-09 00:57:12 -0300

    comment service 3

--- moderation/index.js
+++ moderation/index.js
@@ -5,13 +5,13 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/event', async (req, res) => {
-    const { data, type } = req.body;
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
 
     if (type === 'CommentCreated') {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-        await axios.post('http://192.168.1.104:3000/events', {
+        await axios.post('http://192.168.1.104:4005/events', {
             type: 'CommentModerated',
             data: {
                 id: data.id,
